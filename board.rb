94560732519988e81b1d7a6f99cbda1f3bd365d6
Aaron Horton <aaronmichaelhorton@gmail.com>
2014-07-17 11:43:05 -0400

    Add display method

--- board.rb
+++ board.rb
@@ -17,25 +17,6 @@ class Board
     self.grid[pos.last][pos.first] = piece
   end
 
-  def populate_board
-    @grid.each_with_index do |row, y|
-
-      row.each_index do |x|
-
-        if (y == 0 || y == 2) && x % 2 != 0
-          Piece.new(self, [x,y], :l)
-        elsif y == 1 && x % 2 == 0
-          Piece.new(self, [x,y], :l)
-        elsif (y == 5 || y == 7) && x % 2 == 0
-          Piece.new(self, [x,y], :d)
-        elsif y == 6 && x % 2 != 0
-          Piece.new(self, [x,y], :d)
-        end
-
-      end
-    end
-  end
-
   def move(start_pos, end_pos)
     if self[start_pos].moves.include?(end_pos)
       self[end_pos] = self[start_pos]
@@ -57,10 +38,36 @@ class Board
     false
   end
 
+  def display
+    p grid
+  end
+
+  private
+
+  def populate_board
+    @grid.each_with_index do |row, y|
+
+      row.each_index do |x|
+
+        if (y == 0 || y == 2) && x % 2 != 0
+          Piece.new(self, [x,y], :l)
+        elsif y == 1 && x % 2 == 0
+          Piece.new(self, [x,y], :l)
+        elsif (y == 5 || y == 7) && x % 2 == 0
+          Piece.new(self, [x,y], :d)
+        elsif y == 6 && x % 2 != 0
+          Piece.new(self, [x,y], :d)
+        end
+
+      end
+    end
+  end
+
 
 end
 
 my_board = Board.new
 francis = Piece.new(my_board, [5,4], :l)
-p my_board.grid
-p my_board.jump_available?(:l)
+my_board.display
+my_board.move([5,4], [6,7])
+my_board.display
